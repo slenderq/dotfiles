c1a0c0dfe8430d255a7dc7d9db758659dfdf4e09
justin <justin.quaintance@protonmail.com>
2022-03-29 14:24:22 -0600

    Making sure I can get my vim plugin's in other spots

---
+++ install_vim.sh
@@ -0,0 +1,8 @@
+# Vim plugins
+mkdir -p /home/vscode/.vim/pack/plugins/start
+git clone --depth=1 https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp
+
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
+
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+
